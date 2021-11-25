461bc8b8bf6f98d09620fe040fdb057975c91434
MatMasIt <mattia.mascarello@outlook.it>
2021-11-25 18:36:05 +0100

    Logic now depends on network availability

--- epg.sh
+++ epg.sh
@@ -1,9 +1,8 @@
 sudo apt install php wget -y
-mv programmi/* archivio/$(date '+%Y-%m-%d')
-mkdir programmi
 wget https://iptv-org.github.io/epg/guides/it/guidatv.sky.it.epg.xml
 if [ $? -ne 0 ]; then
     exit
 fi
+mv programmi/* archivio/$(date '+%Y-%m-%d')
 mv guidatv.sky.it.epg.xml.1 guidatv.sky.it.epg.xml
 php xmlParseProgrammeList.php
