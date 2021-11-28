80b9893b58e3d419f391bc2fea8dfdec6d006cbf
MatMasIt <mattia.mascarello@outlook.it>
2021-11-28 13:31:39 +0100

    Fixed dir creation

--- epg.sh
+++ epg.sh
@@ -3,6 +3,7 @@ wget https://iptv-org.github.io/epg/guides/it/guidatv.sky.it.epg.xml
 if [ $? -ne 0 ]; then
     exit
 fi
+mkdir -p archivio/$(date '+%Y-%m-%d')
 mv programmi/* archivio/$(date '+%Y-%m-%d')
 mv guidatv.sky.it.epg.xml.1 guidatv.sky.it.epg.xml
 php xmlParseProgrammeList.php
