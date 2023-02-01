ee1a15c88190bbcd6c02aa372df583bea3f11a70
Mattia Mascarello <mattia.mascarello@outlook.it>
2023-02-02 00:02:18 +0100

    aggiornato url

--- epg.sh
+++ epg.sh
@@ -1,9 +1,9 @@
 sudo apt install php wget -y
-wget https://iptv-org.github.io/epg/guides/it/guidatv.sky.it.epg.xml
+wget https://iptv-org.github.io/epg/guides/it/guidatv.sky.it.xml
 if [ $? -ne 0 ]; then
     exit
 fi
 mkdir -p archivio/$(date '+%Y-%m-%d')
 mv programmi/* archivio/$(date '+%Y-%m-%d')
-mv guidatv.sky.it.epg.xml.1 guidatv.sky.it.epg.xml
+mv guidatv.sky.it.xml.1 guidatv.sky.it.xml
 php xmlParseProgrammeList.php
